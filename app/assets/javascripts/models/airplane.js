2c6a656837e8ac14d29366d4fab9e99ac349aed4
Mario Martinez <zenitram.oiram@gmail.com>
2015-03-14 11:42:48 +1100

    airplane url set

--- app/assets/javascripts/models/airplane.js
+++ app/assets/javascripts/models/airplane.js
@@ -1,6 +1,9 @@
 var app = app || {};
 
 app.Airplane = Backbone.Model.extend({
+  urlRoot: function () {
+    return '/flights/' + this.get('flight_id') + '/airplane';
+  },
   defaults: {
     name: 'New Plane',
     row: 0,
